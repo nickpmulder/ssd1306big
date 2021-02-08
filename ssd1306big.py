91deaf518c5324e92800d64cb2fb3eac86d43478
nickpmulder <61099991+nickpmulder@users.noreply.github.com>
2021-02-08 04:25:19 +0000

    Update ssd1306big.py

--- ssd1306big.py
+++ ssd1306big.py
@@ -2,6 +2,8 @@
 # This was written for a Raspberry Pi Pico but should work on any microcontroller running micropython. 
 # It was based originally on the ssd1306.py module, but a new, larger font was drawn using framebuffer lines by Nick Mulder 
 # This module is open source, and can be used for free for any purpose. 
+# The default wiring is SDA to GP8 and SCL to GP9 for Rasberry Pi Pico
+
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 # The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
