60434b40cbb2735552dd55204ee61244b5fab705
nickpmulder <61099991+nickpmulder@users.noreply.github.com>
2021-02-07 22:57:53 -0500

    write comments at beginning of code

--- ssd1306big.py
+++ ssd1306big.py
@@ -1,3 +1,11 @@
+# This is a driver for ssd1306 i2c oled displays using micropython. 
+# This was written for a Raspberry Pi Pico but should work on any microcontroller running micropython. 
+# It was based originally on the ssd1306.py module, but a new, larger font was drawn using framebuffer lines by Nick Mulder 
+# This module is open source, and can be used for free for any purpose. 
+
+
+
+
 import machine
 import time
 
